b90b1b5274eb39cfdb0562d784baa0448388b531
Juanchoox <32881812+Juanchoox@users.noreply.github.com>
2017-10-24 17:33:59 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -9,6 +9,10 @@ class EstacionTest extends TestCase {
     /**
      * Comprueba que el saldo de una tarjeta nueva sea cero.
      */
+    public function testTransporte() {
+        $bici = new Transporte("bici", "123456", "12:30", "4 de mayo");
+    }
+           
     public function testSaldoCero() {
         $tarjeta = new Tarjeta;
         $tarjeta1 = new Tarjeta;
@@ -17,8 +21,10 @@ class EstacionTest extends TestCase {
         $tarjeta->cargarSaldo(50);
         $this->assertEquals($tarjeta->saldo(), 50);
         $tarjeta1->cargarSaldo(332);
-        $this->assertEquals($tarjeta1->saldo(),388);
-    }
-    public function testTransporte() {
+        $this->assertEquals($tarjeta1->saldo(), 388);
         
+        $tarjeta1->sacarBici($bici);
+        $this->assertEquals($tarjeta1->saldo(), 375.25);
+        $this->assertEquals($tarjeta1->saldo(), 375.25);
+    }
 }
