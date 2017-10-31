11a3fdbd307c29abfe0bd1c99a61990af207df46
Frodriguez13 <32495491+Frodriguez13@users.noreply.github.com>
2017-10-31 17:13:33 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -6,25 +6,28 @@ use PHPUnit\Framework\TestCase;
 
 class EstacionTest extends TestCase {
 
-    /**
-     * Comprueba que el saldo de una tarjeta nueva sea cero.
-     */
-    public function testTransporte() {
-        $bici = new Transporte("bicicleta", "123456", "4 de mayo");
-    }
-           
     public function testSaldo() {
         $tarjeta = new Tarjeta;
         $tarjeta1 = new Tarjeta;
 
         $this->assertEquals($tarjeta->saldo(), 0);
+        //saldo de la tarjeta = 0
         $tarjeta->cargarSaldo(50);
         $this->assertEquals($tarjeta->saldo(), 50);
+        //cargar $50
         $tarjeta1->cargarSaldo(332);
         $this->assertEquals($tarjeta1->saldo(), 388);
+        //cargar $332 y se acreditan $388    
+    }
+    
+    public function testBici() {
+        $bici = new Transporte("bicicleta", "123456", "4 de mayo");
         
-        $tarjeta1->sacarBici($bici);
+        $tarjeta1->abonarViaje($bici);
         $this->assertEquals($tarjeta1->saldo(), 375.25);
+        //cobra primer viaje en bici
+        $tarjeta1->abonarViaje($bici);
         $this->assertEquals($tarjeta1->saldo(), 375.25);
+        //no cobra otro viaje el mismo dia
     }
 }
