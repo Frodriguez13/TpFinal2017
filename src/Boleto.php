e37e5e8eb78b87e45f987fa8a5087b1d10b505cd
Frodriguez13 <32495491+Frodriguez13@users.noreply.github.com>
2017-11-02 09:29:41 -0300

    Update Boleto.php

--- src/Boleto.php
+++ src/Boleto.php
@@ -7,13 +7,24 @@ class Boleto {
     protected $dia;
     protected $tipoBoleto;
     protected $saldo;
-    protected $id;
     protected $lineaVehiculo;
     
-    public function __construct($a, $b, $c, $d, $e) {
+    public function __construct($a, $b, $c, $d) {
         $this->dia = $d;
         $this->tipoBoleto = $a;
         $this->saldo = $b;
-        $this->id = $e;
         $this->lineaVehiculo = $c;
     }
+ 
+    public function obtenerBoleto(){
+          return $this->tipo;
+       }
+       public function obtenerSaldo(){
+          return $this->monto;
+       }
+       public function obtenerLinea(){
+          return $this->transporte;
+       }
+       public function obtenerFecha() {
+           return $this->fecha_y_hora;
+       }
