269af11e9273b25c4f7d9d0634a274e6e8744b35
Frodriguez13 <32495491+Frodriguez13@users.noreply.github.com>
2017-11-02 10:58:24 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -21,14 +21,14 @@ class EstacionTest extends TestCase {
     }
     
     public function testBici() {
-        $bici = new Transporte("bicicleta", "123456", "4 de mayo");
+        $bici = new bici("123456");
         $tarjeta = new Tarjeta();
         $tarjeta->cargarSaldo(50);
         
-        $tarjeta->abonarViaje($bici);
+        $tarjeta->abonarViaje($bici,'10/07/2017 15:30');
         $this->assertEquals($tarjeta1->saldo(), 38.25);
         //cobra primer viaje en bici
-        $tarjeta->abonarViaje($bici);
+        $tarjeta->abonarViaje($bici,'10/07/2017 15:45');
         $this->assertEquals($tarjeta1->saldo(), 38.25);
         //no cobra otro viaje el mismo dia
     }
